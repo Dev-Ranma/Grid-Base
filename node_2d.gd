71241b9ccebe40fbaae83723d9a027a7447b7873
Dev-Ranma <DevRanma@protonmail.com>
2025-08-06 06:25:21 +0000

    Version 1.2
    Changed the matrix and true/false grid cell fill system, autorun time still work in progress

--- node_2d.gd
+++ node_2d.gd
@@ -4,6 +4,7 @@ var cellCoordsX : Array = [1]
 var cellCoordsY : Array = [1]
 var cellCoordsBool : PackedByteArray = []
 var cellCoordsTrue : PackedVector2Array = []
+var cellGridIndex : Array[Array] # 0: X-Coordinate, 1: Y-Coordinate, 2: Cell Fill
 var GridX : float = 600.0
 var GridY : float = 600.0
 var cellNum : int = 3 #150 is hard limit for number of grid lines
@@ -11,6 +12,7 @@ var areLinesActive : bool = true
 var areCellsFilled : bool = true
 var autorunPre : bool
 var autorunPost : bool
+@onready var timer = $Timer
 
 
 func _ready() -> void:
@@ -33,10 +35,8 @@ func _draw() -> void:
 	draw_rect(Rect2(0.0, 0.0, GridX, GridY), Color.WHITE, true, -1)
 	
 	while i < (cellNum * cellNum):
-		if cellCoordsBool[i] == 1:
-			pass
-			#print(((float(cellCoordsTrue[i].x) / float(cellNum)) * 600 ), ", ", ((float(cellCoordsTrue[i].y) / float(cellNum)) * 600))
-			draw_rect(Rect2((((cellCoordsTrue[i].x - 1.0) / float(cellNum)) * 600 ), (((cellCoordsTrue[i].y - 1.0)/ float(cellNum)) * 600 ), cellWidth, cellHeight), Color.BLACK, true)
+		if cellGridIndex[i][2] == 1:
+			draw_rect(Rect2((((cellGridIndex[i][0] - 1.0) / float(cellNum)) * 600 ), (((cellGridIndex[i][1] - 1.0)/ float(cellNum)) * 600 ), cellWidth, cellHeight), Color.BLACK, true)
 		i += 1
 	
 	draw_rect(Rect2(0.0, 0.0, GridX, GridY), Color.RED, false, 4)
@@ -54,13 +54,16 @@ func _draw() -> void:
 
 func _grid_points():
 	var i : int = 1
-	while i < (cellNum):
-		i += 1
-		cellCoordsX.append(i)
-		cellCoordsY.append(i)
-	cellCoordsTrue.resize((cellNum * cellNum))
-	cellCoordsBool.resize((cellNum * cellNum))
-	cellCoordsBool.fill(0)
+	var x : int = 0
+	var y : int = 0
+	
+	for cell in (cellNum * cellNum):
+		cellGridIndex.resize(cellNum * cellNum)
+		cellGridIndex[cell] = [(x+1), (y+1), 0]
+		x += 1
+		if x == cellNum:
+			x = 0
+			y += 1
 
 func _cell_fill_in():
 	var randCellsX : PackedInt32Array = []
@@ -72,8 +75,10 @@ func _cell_fill_in():
 	while i < numOfFills:
 		i += 1
 		currentRand = randi_range(0, ((cellNum * cellNum) - 1))
-		cellCoordsBool[currentRand] = true
-		cellCoordsTrue[currentRand] = Vector2(randi_range(1, cellNum), randi_range(1, cellNum))
+		cellGridIndex[currentRand][2] = 1
+		#cellCoordsBool[currentRand] = true
+		#cellCoordsTrue[currentRand] = Vector2(randi_range(1, cellNum), randi_range(1, cellNum))
+	#print(cellGridIndex)
 
 func _ret_to_go():
 	_grid_points()
@@ -82,14 +87,13 @@ func _ret_to_go():
 	queue_redraw()
 
 func _on_generate_button_pressed() -> void:
-	pass
-	_ret_to_go()
 	if autorunPre == true:
 		autorunPost = true
 		#print(autorunPost)
 	else:
 		autorunPost = false
 		#print(autorunPost)
+	_ret_to_go()
 
 func _on_grid_num_input_text_changed(new_text: String) -> void:
 	cellNum = clampi(int(new_text), 2, 150)
@@ -104,3 +108,6 @@ func _on_fill_cells_check_toggled(toggled_on: bool) -> void:
 func _on_autorun_check_toggled(toggled_on: bool) -> void:
 	autorunPre = toggled_on
 	#print(autorunPre)
+
+func _on_timer_timeout() -> void:
+	pass
